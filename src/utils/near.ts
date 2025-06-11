34dbd38a492f0ea7d6f6da9538daee24f1fde3e6
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-06-11 16:50:43 +0300

    feat: proposal creation

--- src/utils/near.ts
+++ src/utils/near.ts
@@ -1,5 +1,20 @@
-import { type FinalExecutionOutcome } from '@near-js/types';
 import * as nearApi from 'near-api-js';
+import { Account, Near } from 'near-api-js';
+import { type FinalExecutionOutcome } from 'near-api-js/lib/providers';
+import { type ExecutionStatus } from 'near-api-js/lib/providers/provider';
+import { type KeyPairString } from 'near-api-js/lib/utils';
+
+import { type Token } from '@/constants/tokenList';
+
+const nearAccount = process.env.NEAR_ACCOUNT || 'nearn-io.near';
+const nearAccountPrivateKey = process.env.NEAR_ACCOUNT_PRIVATE_KEY || '';
+
+const keyStore = new nearApi.keyStores.InMemoryKeyStore();
+keyStore.setKey(
+  'mainnet',
+  nearAccount,
+  nearApi.KeyPair.fromString(nearAccountPrivateKey as KeyPairString),
+);
 
 const jsonProviders = [
   new nearApi.providers.JsonRpcProvider(
@@ -17,7 +32,10 @@ const connectionConfig = {
   networkId: 'mainnet',
   nodeUrl: 'https://rpc.mainnet.near.org',
   provider,
+  keyStore,
 };
+
+const near = new Near(connectionConfig);
 
 export async function getTransactionStatus(
   accountId: string,
@@ -46,4 +64,91 @@ export function formatTokenAmount(amount: string, decimals: number): string {
   const [wholePart, decimalPart = ''] = amount.split('.');
   const paddedDecimal = decimalPart.padEnd(decimals, '0');
   return `${wholePart}${paddedDecimal}`;
+}
+
+export async function getStorageBalance(
+  tokenAddress: string,
+  accountId: string,
+) {
+  const api = await nearApi.connect(connectionConfig);
+
+  const account = new Account(api.connection, accountId);
+
+  const result: { total: string; used: string } | null =
+    await account.viewFunction({
+      contractId: tokenAddress,
+      methodName: 'storage_balance_of',
+      args: {
+        account_id: accountId,
+      },
+    });
+
+  return result;
+}
+
+export async function createSputnikProposal(
+  dao: string,
+  description: string,
+  token: Token,
+  receiver: string,
+  amount: string,
+) {
+  const args = {
+    proposal: {
+      description,
+      kind: {
+        Transfer: {
+          token_id: token.tokenSymbol === 'NEAR' ? '' : token.mintAddress,
+          receiver_id: receiver,
+          amount: amount,
+        },
+      },
+    },
+  };
+
+  const account = await near.account(nearAccount);
+
+  const daoPolicy: { proposal_bond: string | undefined } =
+    await account.viewFunction({
+      contractId: dao,
+      methodName: 'get_policy',
+    });
+
+  const calls: unknown[] = [
+    {
+      contractId: dao,
+      methodName: 'add_proposal',
+      args,
+      gas: BigInt(300000000000000),
+      attachedDeposit: BigInt(daoPolicy.proposal_bond || 0),
+    },
+  ];
+
+  if (!(await getStorageBalance(token.mintAddress, receiver))) {
+    const depositInYocto = BigInt(125) * BigInt(10) ** BigInt(21);
+
+    calls.push({
+      contractId: token.mintAddress,
+      methodName: 'storage_deposit',
+      args: {
+        account_id: receiver,
+        registration_only: true,
+      },
+      gas: BigInt(300000000000000),
+      attachedDeposit: depositInYocto,
+    });
+  }
+
+  const result = await Promise.all(
+    calls.map((call) => account.functionCall(call as any)),
+  );
+
+  return getProposalId(result[0]!);
+}
+
+export async function getProposalId(result: FinalExecutionOutcome) {
+  const base64 = (result.receipts_outcome[0]?.outcome.status as ExecutionStatus)
+    .SuccessValue;
+  const proposalId = Buffer.from(base64!, 'base64').toString('utf-8');
+  return proposalId;
 }
