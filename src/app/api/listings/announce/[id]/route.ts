7b24d2146c9de65936a77a82d3779163981bbdb2
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-06-25 14:12:40 +0300

    feat: moved approve date to proper place

--- src/app/api/listings/announce/[id]/route.ts
+++ src/app/api/listings/announce/[id]/route.ts
@@ -143,8 +143,6 @@ export async function POST(
           data: {
             rewardInUSD,
             status: 'Approved',
-            approveDate: new Date(),
-            approvedBy: userId,
             label:
               winners[currentIndex]?.label === 'New'
                 ? 'Reviewed'
