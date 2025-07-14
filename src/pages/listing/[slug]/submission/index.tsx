12e2d2861682a2debb735c03557f32c482373a13
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-07-14 17:53:29 +0300

    bugfix: redirect url should point correctly

--- src/pages/listing/[slug]/submission/index.tsx
+++ src/pages/listing/[slug]/submission/index.tsx
@@ -21,7 +21,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   return {
     redirect: {
-      destination: `${getBountyUrl(bountyData?.bounty)}/submissions`,
+      destination: `${getBountyUrl(bountyData?.bounty)}/submission`,
       permanent: true,
     },
   };
