0a5bd2434a493931e98d7140e3b38ebddf2c42dd
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-09-16 18:34:16 +0300

    bugfix: fixed modal

--- src/features/conversion-popups/components/ListingPop.tsx
+++ src/features/conversion-popups/components/ListingPop.tsx
@@ -44,9 +44,10 @@ const VariantInfo = (
     listing?.usdValue?.toFixed(0) ||
     listing?.rewardAmount?.toFixed(0) ||
     listing?.maxRewardAsk?.toFixed(0);
-  const rewardLabel = reward
-    ? '$' + Number(reward).toLocaleString('en-us')
-    : '';
+  const rewardLabel =
+    reward && listing?.compensationType !== 'variable'
+      ? '$' + Number(reward).toLocaleString('en-us')
+      : '';
   const type = listing?.type;
   const verb = listing?.type === 'bounty' ? 'submissions' : 'applications';
   const sponsorName = listing?.sponsor?.name;
