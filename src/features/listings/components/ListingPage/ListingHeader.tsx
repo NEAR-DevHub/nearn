7104c44140f06a1bef6b45d89eb91b8dcfaf4255
mar4enkom <mar44enkom@gmail.com>
2025-10-24 13:44:50 +0200

    refactor: delete unused changes with previous preview bugfix

--- src/features/listings/components/ListingPage/ListingHeader.tsx
+++ src/features/listings/components/ListingPage/ListingHeader.tsx
@@ -278,8 +278,6 @@ export function ListingHeader({
     );
   };
 
-  const isPreviewMode = router.asPath.includes('nsb=1');
-
   return (
     <div className="flex flex-col gap-1 bg-white">
       <div className="mx-auto flex w-full max-w-7xl justify-between gap-5 py-4 md:py-10">
@@ -301,7 +299,7 @@ export function ListingHeader({
             </div>
           </div>
         </div>
-        {listing.id && !isPreviewMode && (
+        {listing.id && (
           <div className="flex items-center gap-2">
             <SubscribeListing isTemplate={isTemplate} id={listing.id} />
             <AuthWrapper sponsorId={listing.sponsorId}>
